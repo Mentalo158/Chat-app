25857f91e81b23e1a0874297bd30adc9351d1078
Twistee <93496053+Twistee@users.noreply.github.com>
2022-11-19 22:20:18 +0100

    @

---
+++ lib/screens/models/nachrichten.dart
@@ -0,0 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_course/Screens/Models/User.dart';
+import 'package:flutter_course/screens/login/AuthScreen.dart';
+import 'package:flutter_course/screens/login/db.dart';
+
+
+class Nachricht{
+String? content;
+String? uid;
+String? senderUID;
+String? reciverUID;
+Timestamp? createAt;
+
+Nachricht({this.content, this.senderUID, this.reciverUID, this.createAt, this.uid});
+
+Nachricht.fromJson(Map<String, dynamic> json, String id){
+  uid = id;
+  content = json["conte"];
+  senderUID = json["senderUID"];
+  reciverUID = json["reciverUID"];
+  createAt = json["createAt"];
+}
+  Map<String, dynamic> toJson(){
+  return {
+    "content": content,
+    "senderUID": senderUID,
+    "reciverUID": reciverUID,
+    "createAt": createAt
+  };
+  }
+  bool get isMe => AuthScreen().user.uid == senderUID;
+
+}
