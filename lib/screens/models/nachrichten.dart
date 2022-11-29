a31075863edfdd6443c618bba505553051f872da
Stewart <Stewartbenyamin12345@gmail.com>
2022-11-29 18:09:59 +0100

    hh

--- lib/screens/models/nachrichten.dart
+++ lib/screens/models/nachrichten.dart
@@ -1,8 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter_course/Screens/Models/User.dart';
-import 'package:flutter_course/screens/login/AuthScreen.dart';
-import 'package:flutter_course/screens/login/db.dart';
 
 class Nachricht {
   String? content;
