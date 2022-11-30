86e4f863efc248d2cfb70eac1cb649fd05bb3957
Stewart <Stewartbenyamin12345@gmail.com>
2022-11-30 15:30:51 +0100

    chat funktioniert jetzt

--- lib/screens/models/nachrichten.dart
+++ lib/screens/models/nachrichten.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class Nachricht {
-  String? content;
+  String content;
   String? uid;
   String? senderUID;
   String? reciverUID;
@@ -11,23 +11,26 @@ class Nachricht {
   final userId = FirebaseAuth.instance.currentUser!.uid;
 
   Nachricht(
-      {this.content, this.senderUID, this.reciverUID, this.createAt, this.uid});
+      {required this.content,
+      this.senderUID,
+      this.reciverUID,
+      this.createAt,
+      this.uid});
 
-  Nachricht.fromJson(Map<String, dynamic> json, String id) {
-    uid = id;
-    content = json["conte"];
-    senderUID = json["senderUID"];
-    reciverUID = json["reciverUID"];
-    createAt = json["createAt"];
-  }
-  Map<String, dynamic> toJson() {
-    return {
-      "content": content,
-      "senderUID": senderUID,
-      "reciverUID": reciverUID,
-      "createAt": createAt
-    };
-  }
+  static Nachricht fromJson(Map<String, dynamic> json, String id) => Nachricht(
+        content: json["content"],
+        senderUID: json["senderUID"],
+        reciverUID: json["receiverUID"],
+        createAt: json["createAt"],
+        uid: json["uid"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "content": content,
+        "senderUID": senderUID,
+        "reciverUID": reciverUID,
+        "createAt": createAt
+      };
 
   bool get isMe => userId == senderUID;
 }
--- lib/widgets/NachrichtenWidg.dart
+++ lib/widgets/NachrichtenWidg.dart
@@ -2,38 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:flutter_course/screens/models/nachrichten.dart';
 
 class NachrichtenWid extends StatelessWidget {
-  const NachrichtenWid({Key? key, this.msg}) : super(key: key);
-  final Nachricht? msg;
+  const NachrichtenWid({Key? key, required this.msg}) : super(key: key);
+  final Nachricht msg;
 
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
-    var date = msg!.createAt!.toDate().toLocal();
+    var date = msg.createAt!.toDate().toLocal();
     return Row(
       mainAxisAlignment:
-          msg!.isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+          msg.isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
       children: [
         Stack(
           children: [
             Container(
               padding: EdgeInsets.all(5),
               decoration: BoxDecoration(
-                  color: msg!.isMe
+                  color: msg.isMe
                       ? Colors.blueAccent
                       : Colors.black.withOpacity(.8),
                   borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(10),
                     topRight: Radius.circular(10),
                     bottomLeft:
-                        msg!.isMe ? Radius.circular(10) : Radius.circular(0),
+                        msg.isMe ? Radius.circular(10) : Radius.circular(0),
                     bottomRight:
-                        msg!.isMe ? Radius.circular(0) : Radius.circular(10),
+                        msg.isMe ? Radius.circular(0) : Radius.circular(10),
                   )),
               constraints: BoxConstraints(
                   minWidth: 30, minHeight: 40, maxWidth: width / 1.1),
               child: Text(
                 // mst!.content! sorgt für fehler muss geändert werden(Null Check)
-                msg!.content!,
+                msg.content,
                 textAlign: TextAlign.start,
                 style: TextStyle(color: Colors.white),
               ),
