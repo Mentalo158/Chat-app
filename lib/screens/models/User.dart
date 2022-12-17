5dd56dfc26fe0071edab81fbd81c72d84daf01ce
Stewart <Stewartbenyamin12345@gmail.com>
2022-12-17 23:37:06 +0100

    save

--- lib/screens/models/User.dart
+++ lib/screens/models/User.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class MyUser {
-  String? uid;
+  String uid;
   String username;
   String email;
   DateTime birthday;
@@ -11,7 +11,7 @@ class MyUser {
   String imagePaths;
 
   MyUser({
-    this.uid,
+    required this.uid,
     required this.username,
     required this.email,
     required this.birthday,
