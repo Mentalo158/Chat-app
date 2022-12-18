e9e92c346f7cf11d8a088da8ca3491482b0a359d
Stewart <Stewartbenyamin12345@gmail.com>
2022-12-18 20:19:26 +0100

    l

--- lib/screens/tabBarScreens/chatScreen.dart
+++ lib/screens/tabBarScreens/chatScreen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_course/screens/login/db.dart';
 import 'package:flutter_course/screens/models/User.dart';
@@ -12,6 +13,8 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final userId = FirebaseAuth.instance.currentUser!.uid;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
